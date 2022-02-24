8f8aa2521593b70b7c5aec35e6a0217451567ca0
Roberto <mayanhavoc@gmail.com>
2022-02-24 13:08:55 -0800

    changes path to gallery images

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -12,52 +12,52 @@ function Gallery() {
         <div className="row" data-masonry='{"percentPosition": true }'>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../../images/libertart-front.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-front.png" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../../images/libertart-beach.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-beach.png" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../../images/libertart-donuts.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-donuts.png" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../../images/libertart-garden.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-garden.png" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../../images/libertart-sign.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-sign.png" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../../images/libertart-pool.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-pool.png" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../../images/libertart-lawnchairs.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-lawnchairs.png" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../../images/libertart-flowegarden.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-flowegarden.png" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../../images/libertart-fort.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-fort.png" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../../images/libertart-breakfast.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-breakfast.png" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
         </div>
