31418e4ccaae9fbc06c8489734dc61468860249a
Roberto <mayanhavoc@gmail.com>
2022-02-24 14:29:17 -0800

    optimizes images and changes format to jpg

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -12,52 +12,52 @@ function Gallery() {
         <div className="row" data-masonry='{"percentPosition": true }'>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../images/libertart-front.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-front.jpg" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../images/libertart-beach.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-beach.jpg" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../images/libertart-donuts.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-donuts.jpg" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../images/libertart-garden.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-garden.jpg" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../images/libertart-sign.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-sign.jpg" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../images/libertart-pool.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-pool.jpg" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../images/libertart-lawnchairs.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-lawnchairs.jpg" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../images/libertart-flowegarden.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-flowergarden.jpg" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../images/libertart-fort.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-fort.jpg" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
           <div className="col-sm-6 col-lg-4 mb-1">
             <div className="card">
-              <img src="../images/libertart-breakfast.png" class="card-img-top rounded" alt="..."/>
+              <img src="../images/libertart-breakfast.jpg" class="card-img-top rounded" alt="..."/>
             </div>
           </div>
         </div>
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -9,32 +9,52 @@ function Reviews() {
           <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mt-2">
             <div class="col">
               <div class="card">
-                <img src="../../images/libertart-review1.png" class="card-img-top rounded" alt="..."/>
+                <img src="../../images/libertart-review1.jpg" class="card-img-top rounded" alt="..."/>
               </div>
             </div>
             <div class="col">
               <div class="card">
-                <img src="../../images/libertart-review2.png" class="card-img-top rounded" alt="..."/>
+                <img src="../../images/libertart-review2.jpg" class="card-img-top rounded" alt="..."/>
               </div>
             </div>
             <div class="col">
               <div class="card">
-                <img src="../../images/libertart-review3.png" class="card-img-top rounded" alt="..."/>
+                <img src="../../images/libertart-review3.jpg" class="card-img-top rounded" alt="..."/>
               </div>
             </div>
             <div class="col">
               <div class="card">
-                <img src="../../images/libertart-review4.png" class="card-img-top rounded" alt="..."/>
+                <img src="../../images/libertart-review4-250h.jpg" class="card-img-top rounded" alt="..."/>
               </div>
             </div>
             <div class="col">
               <div class="card">
-                <img src="../../images/libertart-review3.png" class="card-img-top rounded" alt="..."/>
+                <img src="../../images/libertart-review5.jpg" class="card-img-top rounded" alt="..."/>
               </div>
             </div>
             <div class="col">
               <div class="card">
-                <img src="../../images/libertart-review4.png" class="card-img-top rounded" alt="..."/>
+                <img src="../../images/libertart-review6.jpg" class="card-img-top rounded" alt="..."/>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <img src="../../images/libertart-review7.jpg" class="card-img-top rounded" alt="..."/>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <img src="../../images/libertart-review8.jpg" class="card-img-top rounded" alt="..."/>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <img src="../../images/libertart-review9.jpg" class="card-img-top rounded" alt="..."/>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card">
+                <img src="../../images/libertart-review10.jpg" class="card-img-top rounded" alt="..."/>
               </div>
             </div>
           </div>
