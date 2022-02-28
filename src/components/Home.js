f4846a67915ce5d9ff57af0de56aa0fe2a854ebd
Roberto <mayanhavoc@gmail.com>
2022-02-28 11:43:09 -0800

    adds tagline

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,14 @@ function Home() {
     <>
     <div className='overlay'></div>
     <section id='home' className='vh-100 landing'>
+      <div className="tagline">
+        <h1>Come find the peace you seek in your daily life</h1>
+        <button className="btn btn-success">
+          <a className="text-white" href="" target="_blank">
+            Book Now
+          </a>
+        </button>
+      </div>
     </section>
     </>
   )
